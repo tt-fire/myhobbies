c9f78493b02b195e3c3cd7b7b36620f7651bb8cb
T2B <finbabil@gmail.com>
2020-07-28 05:18:48 +0200

    ui + bootstrap + vue + npm install

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hallo - Laravel
                 </div>
 
                 <div class="links">
