912d3eaf3d4a77eca0c635f02b45af3c0a2814a2
T2B <finbabil@gmail.com>
2020-07-29 06:28:52 +0200

    Abschnitt 13 - zwei statische Seiten angelegt, Navigationshighlight

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('startseite');
+});
+
+Route::get('/info', function () {
+    return view('info');
 });
 
 Auth::routes();
