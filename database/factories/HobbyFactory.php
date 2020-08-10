bcb9b5d1d8ad7f6e007eef877400bf649d55ecd4
T2B <finbabil@gmail.com>
2020-08-10 22:08:13 +0200

    Abschnitt 46 - start seeder für Hobby angelegt

--- database/factories/HobbyFactory.php
+++ database/factories/HobbyFactory.php
@@ -8,5 +8,7 @@ use Faker\Generator as Faker;
 $factory->define(Hobby::class, function (Faker $faker) {
     return [
         //
+        'name' => $faker->realText(30), //min 30 zeichen
+        'beschreibung' => $faker->realText() //Standardwert - ca 200 Zeichen ...
     ];
 });
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -12,6 +12,27 @@ class UserSeeder extends Seeder
     public function run()
     {
         //Aufruf der FAktory für 100 zufalls user!
-        factory(App\User::class, 100)->create();
+        factory(App\User::class, 100)->create()
+        ->each(function ($user){ // Für jeden User 1-7 Hobbies
+            factory(App\Hobby::class,rand(1,8))->create(
+                [
+                    'user_id' => $user->id
+                ]
+            );           
+        });
+        /*
+        ->​ each​ (function ($hobby){ // Für jedes Hobby 1-8 unterschiedl. Tags
+        $tag_ids = range(1,8);
+        shuffle($tag_ids);
+        $verknuepfungen = array_slice($tag_ids, 0, rand(0,8));
+        foreach ($verknuepfungen as $value) {
+        DB::table('hobby_tag')
+        ->insert([
+        'hobby_id' => $hobby->id,
+        'tag_id' => $value,
+        'created_at' => Now(),
+        'updated_at' => Now(),
+        }
+        */
     }
 }
